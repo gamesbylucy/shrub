1b3c1163b0a2b87ed9bec90cbf9b519666c52126
gamesbylucy <gamesbylucy@gmail.com>
2017-10-17 18:02:28 -0500

    still working on experimental
    -added support for finding neighbor nodes
     --will eventually lead to using neighbor nodes to help calculate geometry of the world
    
    -added support method for node diagnostics.

--- Assets/Library/Experimental/Experimental_Landscape.cs
+++ Assets/Library/Experimental/Experimental_Landscape.cs
@@ -8,16 +8,41 @@ public class Experimental_Landscape : MonoBehaviour {
     public Vector3[] vertices;
     public Vector2[] uv;
     public Vector4[] tangents;
+    public Node[,] nodes;
+    public int[] triangles;
 
     private MeshFilter m_meshFilter;
     private MeshRenderer m_meshRenderer;
     private MeshCollider m_meshCollider;
     private Mesh m_mesh;
 
+
+    public struct Node
+    {
+        public int vertexIndex; //index of associated vertex
+        public List<Node> neighbors; //TBD neighbor ordering
+        public int latitude; //0 to mapHeight
+        public int longitude; //0 to mapWidth
+    }
+
+    private static int[][] directions = new int[][] 
+    {
+        new int[]{ -1, -1 },
+        new int[] { -1, 0 },
+        new int[] { -1, 1 },
+        new int[] { 0, 1 },
+        new int[] { 1, 1 },
+        new int[] { 1, 0 },
+        new int[] { 1, -1 },
+        new int[] { 0, -1 }
+    };
+
+    
+
     private void Awake()
     {
-        reposition();
         generate();
+        nodeReport();
     }
     // Use this for initialization
     void Start () {
@@ -42,6 +67,7 @@ public class Experimental_Landscape : MonoBehaviour {
         vertices = new Vector3[(mapWidth + 1) * (mapHeight + 1)];
         uv = new Vector2[vertices.Length];
         tangents = new Vector4[vertices.Length];
+        nodes = new Node[mapWidth + 1, mapHeight + 1];
         Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
 
         //fill the vertices array with Vector3s according to their positions on the grid
@@ -49,9 +75,22 @@ public class Experimental_Landscape : MonoBehaviour {
         {
             for (int currentWidth = 0; currentWidth <= mapWidth; currentWidth++, i++)
             {
-                vertices[i] = new Vector3(currentWidth, currentHeight);
+                vertices[i] = new Vector3(currentWidth, 0, currentHeight);
+                Node theNode = new Node();
+                theNode.vertexIndex = i;
+                theNode.latitude = currentWidth;
+                theNode.longitude = currentHeight;
+                nodes[currentHeight, currentWidth] = theNode;
                 uv[i] = new Vector2((float)currentWidth / mapWidth, (float)currentHeight / mapHeight);
                 tangents[i] = tangent;
+            }
+        }
+
+        for(int i = 0; i < nodes.GetLength(0); i++)
+        {
+            for (int j = 0; j < nodes.GetLength(1); j++)
+            {
+                nodes[i, j].neighbors = getNeighbors(nodes, i, j);
             }
         }
 
@@ -59,15 +98,51 @@ public class Experimental_Landscape : MonoBehaviour {
         m_mesh.uv = uv;
         m_mesh.tangents = tangents;
 
-        int[] triangles = new int[mapWidth * mapHeight * 6];
+        triangles = new int[mapWidth * mapHeight * 6];
         for (int currentTriangle = 0, currentVertex = 0, y = 0; y < mapHeight; y++, currentVertex++)
         {
             for (int x = 0; x < mapWidth; x++, currentTriangle += 6, currentVertex++)
             {
-                triangles[currentTriangle] = currentVertex;
-                triangles[currentTriangle + 3] = triangles[currentTriangle + 2] = currentVertex + 1;
-                triangles[currentTriangle + 4] = triangles[currentTriangle + 1] = currentVertex + mapWidth + 1;
+                triangles[currentTriangle] = currentVertex; //southwest vertex (vertex 1)
+                triangles[currentTriangle + 3] = triangles[currentTriangle + 2] = currentVertex + 1; //southeast vertex (vertex 2)
+                triangles[currentTriangle + 4] = triangles[currentTriangle + 1] = currentVertex + mapWidth + 1; //northwest vertex (vertex 3)
                 triangles[currentTriangle + 5] = currentVertex + mapWidth + 2;
+                /*LEFT TRIANGLE
+                 * vertex 3
+                 * |
+                 * v
+                 * O
+                 * xx
+                 * xxx
+                 * xxxx
+                 * xxxxx
+                 * OxxxxO<- vertex 2
+                 * ^
+                 * |
+                 * vertex 1
+                 *
+                 * RIGHT TRIANGLE
+                 * 
+                 * vertex 2
+                 * |
+                 * v
+                 * OxxxxO <-vertex 1
+                 *  xxxxx
+                 *   xxxx
+                 *    xxx
+                 *     xx
+                 *      O
+                 *      ^
+                 *      |
+                 *      vertex 3
+                 * 
+                 * GRID NEIGHBORS
+                 * [[20][21][22][23][24]
+                 * [15][16][17][18][19]
+                 * [10][11][12][13][14]
+                 * [5 ][6 ][7 ][8 ][9 ]
+                 * [0 ][1 ][2 ][3 ][4 ]]
+                 */
             }
         }
 
@@ -75,24 +150,52 @@ public class Experimental_Landscape : MonoBehaviour {
         m_mesh.RecalculateNormals();
     }
 
-    private void reposition()
-    {
-
-    }
-
     private void OnDrawGizmos()
     {
         //return if vertices have not been initialized to prevent an error
-        if (vertices == null)
+        if (vertices == null || triangles == null)
         {
             return;
         }
 
         //draw all vertices
-        Gizmos.color = Color.black;
         for (int i = 0; i < vertices.Length; i++)
         {
+            Gizmos.color = Color.black;
             Gizmos.DrawSphere(vertices[i], 0.1f);
         }
+    }
+
+    private static List<Node> getNeighbors(Node[ , ] nodes, int x, int y)
+    {
+        List<Node> theNodes = new List<Node>();
+
+        foreach (int[] direction in directions)
+        {
+            int cx = x + direction[0];
+            int cy = y + direction[1];
+            if (cy >= 0 && cy < nodes.GetLength(0))
+            {
+                if (cx >= 0 && cx < nodes.GetLength(1))
+                {
+                    theNodes.Add(nodes[cy, cx]);
+                }
+            }
+        }
+        return theNodes;
+    }
+
+    private void nodeReport()
+    {
+        string arrayString = "";
+        for (int i = 0; i < nodes.GetLength(0); i++)
+        {
+            for (int j = 0; j < nodes.GetLength(1); j++)
+            {
+                arrayString += nodes[i, j].vertexIndex;
+            }
+            arrayString += "\n";
+        }
+        Debug.Log(arrayString);
     }
 }
