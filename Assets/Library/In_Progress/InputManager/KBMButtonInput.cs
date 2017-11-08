fa404dd7512dc4775fa061d2438680a7646fbbae
gamesbylucy <gamesbylucy@gmail.com>
2017-11-08 15:20:12 -0600

    below-the-clouds
    -implemented settable interact buttons for kbm and gamepad
    
    TODO FOR NEXT COMMIT
    -create a ShrubInputEvents class that other classes can subscribe to to change their state depending on the players input.
    -add code to fire events in the processing methods of KBMInput, AxisInput, and GamepadButtonInput
    
    TODO FOR THIS BRANCH
    -implement "below the clouds" mode
    --below the clouds mode increases the detail of a given vertex.
    ---if the vertex is complex, it looks like a city
    ---if a vertex is stable, it looks like a village
    ---if a vertex is populated, it has random agents foraging for food on it
    --will need to create new classes
    ---camera manager and cameras
    ---input manager and inputs
    ---event signals class for transferring input signals to camera manager and game manager.
    ---find a solution for increasing the level of detail of vertices when the camera gets close enough

---
+++ Assets/Library/In_Progress/InputManager/KBMButtonInput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KBMButtonInput : MonoBehaviour, IButtonInput {
+    /**
+     * Public Members
+     */
+    
+    public KeyCode interact
+    {
+        set
+        {
+            m_interact = value;
+        }
+    }
+
+    /**
+     * Private Members
+     */
+     private KeyCode m_interact;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void processInputDown(KeyCode input)
+    {
+        if (input == m_interact)
+        {
+            Debug.Log("Interact was pressed on the keyboard.");
+            /**
+             * Fire event "OnPlayerInteract" here.
+             */
+        }
+    }
+
+    public void processInputHeld(KeyCode input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void processInputUp(KeyCode input)
+    {
+        throw new NotImplementedException();
+    }
+}
